0e2b5c1d573ca9a715838d2db5326c9255b9730c
Treetrain1 <treetrain1@gmail.com>
2024-12-03 18:26:08 -0600

    Reapply "mid datafix"
    This reverts commit 3c7957fcf258faa06eb4b0abf9cc96f9d9df7a61.

--- src/main/java/net/frozenblock/wilderwild/datagen/tag/WWBlockTagProvider.java
+++ src/main/java/net/frozenblock/wilderwild/datagen/tag/WWBlockTagProvider.java
@@ -129,7 +129,9 @@ public final class WWBlockTagProvider extends FabricTagProvider.BlockTagProvider
 			.add(Blocks.DIRT)
 			.add(Blocks.SAND)
 			.add(Blocks.RED_SAND)
+			.add(WWBlocks.SINGED_SAND)
 			.add(WWBlocks.SCORCHED_SAND)
+			.add(WWBlocks.SINGED_RED_SAND)
 			.add(WWBlocks.SCORCHED_RED_SAND);
 	}
 
@@ -313,14 +315,14 @@ public final class WWBlockTagProvider extends FabricTagProvider.BlockTagProvider
 
 		this.getOrCreateTagBuilder(WWBlockTags.SCORCHED_SAND_FEATURE_INNER_REPLACEABLE)
 			.add(Blocks.SAND)
-			.add(WWBlocks.SCORCHED_SAND);
+			.add(WWBlocks.SINGED_SAND);
 
 		this.getOrCreateTagBuilder(WWBlockTags.SCORCHED_SAND_FEATURE_REPLACEABLE)
 			.add(Blocks.SAND);
 
 		this.getOrCreateTagBuilder(WWBlockTags.RED_SCORCHED_SAND_FEATURE_INNER_REPLACEABLE)
 			.add(Blocks.RED_SAND)
-			.add(WWBlocks.SCORCHED_RED_SAND);
+			.add(WWBlocks.SINGED_RED_SAND);
 
 		this.getOrCreateTagBuilder(WWBlockTags.RED_SCORCHED_SAND_FEATURE_REPLACEABLE)
 			.add(Blocks.RED_SAND);
@@ -806,7 +808,9 @@ public final class WWBlockTagProvider extends FabricTagProvider.BlockTagProvider
 			.add(WWBlocks.STONE_CHEST)
 			.add(WWBlocks.NULL_BLOCK)
 			.add(WWBlocks.DISPLAY_LANTERN)
+			.add(WWBlocks.SINGED_SAND)
 			.add(WWBlocks.SCORCHED_SAND)
+			.add(WWBlocks.SINGED_RED_SAND)
 			.add(WWBlocks.SCORCHED_RED_SAND)
 			.add(WWBlocks.GEYSER)
 			.add(WWBlocks.CHISELED_MUD_BRICKS)
@@ -818,7 +822,9 @@ public final class WWBlockTagProvider extends FabricTagProvider.BlockTagProvider
 
 		this.getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_SHOVEL)
 			.addOptionalTag(WWBlockTags.MESOGLEA)
+			.add(WWBlocks.SINGED_SAND)
 			.add(WWBlocks.SCORCHED_SAND)
+			.add(WWBlocks.SINGED_RED_SAND)
 			.add(WWBlocks.SCORCHED_RED_SAND)
 			.add(WWBlocks.TERMITE_MOUND);
 
@@ -841,7 +847,9 @@ public final class WWBlockTagProvider extends FabricTagProvider.BlockTagProvider
 			.addOptionalTag(WWBlockTags.NEMATOCYSTS);
 
 		this.getOrCreateTagBuilder(BlockTags.SAND)
+			.add(WWBlocks.SINGED_SAND)
 			.add(WWBlocks.SCORCHED_SAND)
+			.add(WWBlocks.SINGED_RED_SAND)
 			.add(WWBlocks.SCORCHED_RED_SAND);
 
 		this.getOrCreateTagBuilder(BlockTags.NEEDS_STONE_TOOL)
